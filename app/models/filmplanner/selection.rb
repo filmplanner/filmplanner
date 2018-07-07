a8a9c522b8345c38b68bcd1da80eb5ebe8ddb7e1
Wesley de Bruijn <wesley.dbr@live.nl>
2018-07-08 00:27:06 +0200

    Change selection class

--- app/models/filmplanner/selection.rb
+++ app/models/filmplanner/selection.rb
@@ -1,26 +1,25 @@
 module Filmplanner
   class Selection
-    def initialize(date, theater_ids)
-      @date = date
-      @theater_ids = theater_ids
+    def initialize(date, theater_ids, movie_ids)
+      @date         = date
+      @theater_ids  = theater_ids
+      @movie_ids    = movie_ids
     end
 
     def theaters
       Theater.where(id: @theater_ids)
     end
 
-    def shows
-      Show.includes(:movie).where(date: @date, theater_id: @theater_ids)
+    def movies
+      Movie.where(id: @movie_ids)
     end
 
-    def movies
-      shows.map(&:movie).uniq
+    def shows
+      Show.where(date: @date, theater_id: @theater_ids, movie_id: @movie_ids)
     end
 
     def suggestions
-      theater_key = SuggestionKey.from_keys(theaters.map(&:to_key))
-
-      Suggestion.where(date: @date, theater_key: theater_key.all_keys)
+      Combination.combine(date: date, theater_ids: theater_ids, movie_ids: movie_ids)
     end
   end
 end
