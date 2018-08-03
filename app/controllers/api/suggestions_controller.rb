de882c7c8c9e839393450387cb4e69e581ce0d04
Wesley de Bruijn <wesley.dbr@live.nl>
2018-08-04 01:16:13 +0200

    Cast to integers

--- app/controllers/api/suggestions_controller.rb
+++ app/controllers/api/suggestions_controller.rb
@@ -2,10 +2,24 @@ module Api
   class SuggestionsController < ApplicationController
     def index
       render json: Filmplanner::Combination.combine(
-        date:         params[:date],
-        theater_ids:  Array(params[:theater_ids]),
-        movie_ids:    Array(params[:movie_ids])
+        date:         date,
+        theater_ids:  theater_ids,
+        movie_ids:    movie_ids
       )
+    end
+
+    private
+
+    def date
+      params[:date]
+    end
+
+    def theater_ids
+      Array(params[:theater_ids]).map(&:to_i)
+    end
+
+    def movie_ids
+      Array(params[:movie_ids]).map(&:to_i)
     end
   end
 end
